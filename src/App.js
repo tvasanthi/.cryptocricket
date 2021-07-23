04a8ec629884fb5c46a412c3ac8d92984f908cf2
tvasanthi <tvangel2121@gmail.com>
2021-07-23 11:50:02 +0530

    this is initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      welcome to my react application.
+    </div>
+  );
+}
+
+export default App;
